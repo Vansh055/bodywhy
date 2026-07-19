7bb3fdf290ec103c96b11054e45f9e9722f23a71
Vansh Mahalle <vanshmahale055@gmail.com>
2026-07-19 09:59:57 +0530

    chore: initialize BodyWhy project

---
+++ src/test/java/com/bodywhy/bodywhy/BodywhyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bodywhy.bodywhy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BodywhyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
